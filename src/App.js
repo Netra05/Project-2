dfc2d1bfe3d0e9ad6d66de6ec21bc67023e3bd65
unknown <netravenkatesh05@gmail.com>
2023-03-31 10:15:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Appbar from './Components/Header';
+import Product from './Components/Product'; 
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Appbar />
+      <Product/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BottomNavigation } from '@mui/material';
+
+function Footer() {
+  return (
+    <BottomNavigation>
+    <footer>
+      <div>
+        <h3>Contact Us</h3>
+        <p>123 Main Street<br />Anytown, USA 12345</p>
+        <p>Phone: 555-555-5555<br />Email: info@example.com</p>
+      </div>
+      <div>
+        <h3>Connect With Us</h3>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Instagram</a></li>
+        </ul>
+      </div>
+    </footer>
+    </BottomNavigation>
+  );
+}
+
+export default Footer;
